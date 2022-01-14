e440a7c2d47c6fba53e9eb78633a86000e0222b3
DavidMcLaughlin55 <49570547+DavidMcLaughlin55@users.noreply.github.com>
2022-01-14 18:34:04 -0500

    JS Update-2
    Work on generateModal functionality.

--- js/script.js
+++ js/script.js
@@ -4,10 +4,9 @@ const randomUserAPI = 'https://randomuser.me/api/?results=12';
 const employeeDirectory = document.getElementById('employee-directory');
 const overlay = document.querySelector('.overlay');
 const employeeModal = document.querySelector('.employee-modal')
-const employeeOverlayContent = document.querySelector('.employee-modal-content');
 const overlayBtn = document.querySelector('.close-modal');
 
-
+let employees = [];
 
 //API CONNECTION
 
@@ -23,15 +22,15 @@ fetch(randomUserAPI)
 -Generates employee to be displayed on card along with their details / contact information.
 */
 
+
 function generateEmployees(data) {
 
     employees = data;
-    console.log(employees);
     let employeesHTML = '';
 
 
-    employees.forEach(employee => {
-        let index = employee.index;
+    employees.forEach((employee, index) => {
+
         let picture = employee.picture;
         let firstName = employee.name.first;
         let lastName = employee.name.last;
@@ -40,7 +39,7 @@ function generateEmployees(data) {
         let state = employee.location.state;
 
         employeesHTML += `
-        <div class="card" data="${index}">
+        <div class="card" index="${index}">
             <img class="avatar" src="${picture.large}" alt="member" />
             <div class="employee-info">
                 <h2 class="name">${firstName} ${lastName}</h2>
@@ -52,27 +51,61 @@ function generateEmployees(data) {
     });
 
     employeeDirectory.innerHTML = employeesHTML;
-    return employees
 }
 
 /* generateModal function
 -Generates modal for employee user clicked on to provide further detailed information.
 */
 
-function generateModal(e) {
-    console.log(e.target.data);
-    let employee = e.target.data;
-    // const modalHTML = `
-    // <img class="avatar" src="${}" alt="member">
-    // <div class="employee-info">
-    //     <h2 class="name">${} ${}</h2>
-    //     <p class="email">${}</p>
-    //     <p class="address">${}, ${}</p>
-    //     </hr>
-    //     <p>${}</p>
-    //     <p>${}</p>
-    // `;
+function generateModal(index) {
+    console.log(index);
 
+    let employee = employees[index];
+    console.log(employee);
+
+    let picture = employee.picture;
+    let firstName = employee.name.first;
+    let lastName = employee.name.last;
+    let email = employee.email;
+    let street = employee.location.street;
+    let city = employee.location.city;
+    let state = employee.location.state;
+    let postalCode = employee.location.postcode;
+    let phone = employee.phone;
+    let date = new Date(employee.dob.date);
+
+    console.log(street.name);
+    console.log(date);
+
+
+    const modalHTML = `
+    <img class="avatar" src="${picture.large}" alt="member" />
+    <div class="contact-info">
+        <h2 class="name">${firstName} ${lastName}</h2>
+        <p class="email">${email}</p>
+        <p class="address">${city}</p>
+        </hr>
+        <p class="phone">${phone}</p>
+        <p class="full-address">${street.number} ${street.name}, ${city}, ${state}, ${postalCode}</p>
+        <p class="birthdate">Birthday:${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    </div>
+    `;
+
+    overlay.classList.remove('hidden');
+    employeeModal.innerHTML = modalHTML;
 }
 
-employeeDirectory.addEventListener('click', generateModal)
+/* selectEmployee function
+-When employee card is clicked the index is found and passed into generateModal function
+*/
+
+function selectEmployee(e) {
+    if (e.target !== employeeDirectory) {
+        const card = e.target.closest('.card');
+        const index = card.getAttribute('index');
+        generateModal(index);
+    }
+}
+
+employeeDirectory.addEventListener('click', selectEmployee)
+overlayBtn.addEventListener('click', () => overlay.classList.add('hidden'));
